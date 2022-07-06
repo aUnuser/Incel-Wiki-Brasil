5e4407ad1bf9b1a997ecd4a5a89af3b109b55cc2
Pedro <taskhost.ad@gmail.com>
2022-07-06 17:26:39 -0300

    deploy

--- themes/wiki-theme/assets/js/search.js
+++ themes/wiki-theme/assets/js/search.js
@@ -1,7 +1,8 @@
+var magButton = document.getElementById("searchLabel");
+var searchInputMobile = document.getElementById("searchInputMobile");
+var body = document.getElementsByTagName("body")[0];
+
 document.addEventListener("DOMContentLoaded", function () {
-    var magButton = document.getElementById("searchLabel");
-    var searchInputMobile = document.getElementById("searchInputMobile");
-    var body = document.getElementsByTagName("body")[0];
     if (window.innerWidth < 600) {
         // Mobile
         magButton.addEventListener("click", toggleBodyScroll);
@@ -11,41 +12,46 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
         // Desktop
     }
-    const mobileDesktop = window.matchMedia("(max-width: 1199px)");
-    const bigSmallMobile = window.matchMedia("(min-width: 600px)");
-    mobileDesktop.addEventListener("change", event => {
-        console.log(event);
-        if (event.matches) {
-            // Tablet
-            searchInputMobile.addEventListener("focus", toggleBodyScroll);
-        } else {
-            // Desktop
-            searchInputMobile.removeEventListener("focus", toggleBodyScroll);
-            resetBodyScroll();
-        }
-    });
-    bigSmallMobile.addEventListener("change", event => {
-        console.log(event);
-        if (event.matches) {
-            // Tablet
-            searchInputMobile.addEventListener("focus", toggleBodyScroll);
-            magButton.removeEventListener("click", toggleBodyScroll);
-            resetBodyScroll();
-        } else {
-            // Mobile
-            searchInputMobile.removeEventListener("focus", toggleBodyScroll);
-            magButton.addEventListener("click", toggleBodyScroll);
-            resetBodyScroll();
-        }
-    });
-    function toggleBodyScroll() {
-        body.classList.toggle("noScroll");
+});
+
+/// Media Queries
+const mobileDesktop = window.matchMedia("(max-width: 1199px)");
+const bigSmallMobile = window.matchMedia("(min-width: 600px)");
+mobileDesktop.addEventListener("change", event => {
+    if (event.matches) {
+        // Tablet
+        searchInputMobile.addEventListener("focus", toggleBodyScroll);
+    } else {
+        // Desktop
+        searchInputMobile.removeEventListener("focus", toggleBodyScroll);
+        resetBodyScroll();
     }
-    function resetBodyScroll() {
-        for(var i = 0; i < body.classList.length; i++) {
-            if(body.classList[i] === "noScroll") {
-                body.classList.remove("noScroll")
-            }
+});
+bigSmallMobile.addEventListener("change", event => {
+    if (event.matches) {
+        // Tablet
+        searchInputMobile.addEventListener("focus", toggleBodyScroll);
+        magButton.removeEventListener("click", toggleBodyScroll);
+        resetBodyScroll();
+    } else {
+        // Mobile
+        searchInputMobile.removeEventListener("focus", toggleBodyScroll);
+        magButton.addEventListener("click", toggleBodyScroll);
+        resetBodyScroll();
+        if (document.getElementById("searchToggle").checked) {
+            toggleBodyScroll();
         }
     }
 });
+function toggleBodyScroll() {
+    body.classList.toggle("noScroll");
+}
+function resetBodyScroll() {
+    for (var i = 0; i < body.classList.length; i++) {
+        if (body.classList[i] === "noScroll") {
+            body.classList.remove("noScroll")
+        }
+    }
+}
+
+/// Search
