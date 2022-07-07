b27e5ff0824e156017b03ed1bed09f6ccd6591f4
Pedro <taskhost.ad@gmail.com>
2022-07-07 16:07:00 -0300

    deploy

--- themes/wiki-theme/assets/js/search.js
+++ themes/wiki-theme/assets/js/search.js
@@ -2,6 +2,44 @@ var magButton = document.getElementById("searchLabel");
 var searchInputMobile = document.getElementById("searchInputMobile");
 var body = document.getElementsByTagName("body")[0];
 
+function upDownArrows(event) {
+    var desktopFirst = resultsDesktop.firstChild;
+    var desktopLast = resultsDesktop.lastChild;
+
+    if (event.key === "ArrowUp" && desktopSearchFocused) {
+        event.preventDefault();
+        if (document.activeElement == inputDesktop) {
+            inputDesktop.focus();
+        }
+        else if (document.activeElement == desktopFirst) {
+            inputDesktop.focus();
+        }
+        else {
+            document.activeElement.previousSibling.focus();
+        }
+    } else if (event.key === "ArrowDown" && desktopSearchFocused) {
+        event.preventDefault();
+        if (document.activeElement == inputDesktop) {
+            desktopFirst.focus();
+        }
+        else if (document.activeElement == desktopLast) {
+            desktopLast.focus();
+        }
+        else {
+            document.activeElement.nextSibling.focus();
+        }
+    } else if (event.key === "Enter" && desktopSearchFocused) {
+        window.location.assign(event.target.children[0].href);
+    }
+}
+
+function clickOutside(event) {
+    if ((event.target !== inputDesktop && !inputDesktop.contains(event.target)) && (event.target !== resultsDesktop && !resultsDesktop.contains(event.target))) {
+        resultsDesktop.style.display = "none";
+        desktopSearchFocused = false;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     if (window.innerWidth < 600) {
         // Mobile
@@ -11,6 +49,8 @@ document.addEventListener("DOMContentLoaded", function () {
         searchInputMobile.addEventListener("focus", toggleBodyScroll);
     } else {
         // Desktop
+        document.addEventListener("keydown", upDownArrows);
+        document.addEventListener("click", clickOutside);
     }
 });
 
@@ -21,10 +61,14 @@ mobileDesktop.addEventListener("change", event => {
     if (event.matches) {
         // Tablet
         searchInputMobile.addEventListener("focus", toggleBodyScroll);
+        document.removeEventListener("keydown", upDownArrows);
+        document.removeEventListener("click", clickOutside);
     } else {
         // Desktop
         searchInputMobile.removeEventListener("focus", toggleBodyScroll);
-        resetBodyScroll();
+        resetBodyNoScroll();
+        document.addEventListener("keydown", upDownArrows);
+        document.addEventListener("click", clickOutside);
     }
 });
 bigSmallMobile.addEventListener("change", event => {
@@ -32,12 +76,12 @@ bigSmallMobile.addEventListener("change", event => {
         // Tablet
         searchInputMobile.addEventListener("focus", toggleBodyScroll);
         magButton.removeEventListener("click", toggleBodyScroll);
-        resetBodyScroll();
+        resetBodyNoScroll();
     } else {
         // Mobile
         searchInputMobile.removeEventListener("focus", toggleBodyScroll);
         magButton.addEventListener("click", toggleBodyScroll);
-        resetBodyScroll();
+        resetBodyNoScroll();
         if (document.getElementById("searchToggle").checked) {
             toggleBodyScroll();
         }
@@ -46,7 +90,7 @@ bigSmallMobile.addEventListener("change", event => {
 function toggleBodyScroll() {
     body.classList.toggle("noScroll");
 }
-function resetBodyScroll() {
+function resetBodyNoScroll() {
     for (var i = 0; i < body.classList.length; i++) {
         if (body.classList[i] === "noScroll") {
             body.classList.remove("noScroll")
@@ -69,8 +113,8 @@ var resultsAvailable = false;
 var desktopSearchFocused = false;
 var mobileSearchFocused = false;
 
-document.addEventListener("focus", function(e){
-    if(e.path[0].tagName === "INPUT") {
+document.addEventListener("focus", function (e) {
+    if (e.path[0].tagName === "INPUT") {
         if (firstRun) {
             loadSearch();
             firstRun = false;
@@ -78,51 +122,13 @@ document.addEventListener("focus", function(e){
         if (e.path[0].id === "searchInputMobile") {
 
         } else if (e.path[0].id === "searchInputDesktop") {
-            
-        }
-    } else if (e.path[0].tagName === "LI") {
 
-    } else {
-        resultsDesktop.style.display = "none";
-        desktopSearchFocused = false;
+        }
     }
 }, true);
-document.addEventListener("focusout", function(e) {
-    console.log(e.path[0]);
-});
-
-document.addEventListener('keydown', function (event) {
-    var desktopFirst = resultsDesktop.firstChild;
-    var desktopLast = resultsDesktop.lastChild;
-    if (event.key === "ArrowUp" && desktopSearchFocused) {
-        event.preventDefault();
-        if (document.activeElement == inputDesktop) {
-            inputDesktop.focus();
-        }
-        else if (document.activeElement == desktopFirst) {
-            inputDesktop.focus();
-        }
-        else {
-            document.activeElement.previousSibling.focus();
-        }
-    }
-    if (event.key === "ArrowDown" && desktopSearchFocused) {
-        event.preventDefault();
-        if (document.activeElement == inputDesktop) {
-            desktopFirst.focus();
-        }
-        else if (document.activeElement == desktopLast) {
-            desktopLast.focus();
-        }
-        else {
-            document.activeElement.nextSibling.focus();
-        }
-    }
-});
 
 inputMobile.onkeyup = function (e) {
     executeSearch(this.value, resultsMobile);
-    
 }
 inputDesktop.onkeyup = function (e) {
     executeSearch(this.value, resultsDesktop);
@@ -177,7 +183,7 @@ function executeSearch(term, destination) {
             if (window.innerWidth < 1200) {
                 searchitems = searchitems + "<li><a href=\"" + results[i].item.permalink + "\"><div class=\"thumb\"></div><div class=\"meta\"><h3>" + results[i].item.title + "</h3></div></a></li>";
             } else {
-                searchitems = searchitems + "<li tabindex=\"0\"><a href=\"" + results[i].item.permalink + "\">" + results[i].item.title + "</a></li>";
+                searchitems = searchitems + "<li tabindex=\"0\"><a href=\"" + results[i].item.permalink + "\">" + results[i].item.title.replace(term, "<b>" + term + "</b>") + "</a></li>";
             }
         }
         resultsAvailable = true;
