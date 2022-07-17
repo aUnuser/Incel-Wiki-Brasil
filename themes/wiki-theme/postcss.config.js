bba8b506c90d26c4ea4d50f6649f7682edbc0248
Pedro <taskhost.ad@gmail.com>
2022-07-17 20:16:04 -0300

    deploy

--- themes/wiki-theme/postcss.config.js
+++ themes/wiki-theme/postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     plugins: [
         require("cssnano")({
-            preset: "advanced",
+            preset: ["advanced", {
+                discardComments: {
+                    removeAll: true,
+                },
+            }]
         }),
     ],
 };
